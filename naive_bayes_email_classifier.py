4790bc2da8025f24fe6393e96653a60ea3c45f95
Rohan Sharma <racy.rohan@gmail.com>
2016-10-10 21:00:04 +0530

    converted continuous dataset to discrete calculated all attributes of emails fixed error in email reading

--- naive_bayes_email_classifier.py
+++ naive_bayes_email_classifier.py
@@ -1,11 +1,9 @@
 import base64
 import os
-import time
+import re
 
 import oauth2client
-import re
 from apiclient import errors
-# noinspection PyUnresolvedReferences
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import client
@@ -49,7 +47,7 @@ def get_credentials():
             credentials1 = tools.run_flow(flow, store, flags)
         else:  # Needed only for compatability with Python 2.6
             credentials1 = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+        print 'Storing credentials to ' + credential_path
     return credentials1
 
 
@@ -85,7 +83,7 @@ def list_messages(service1, user_id):
 
         return messages
     except errors.HttpError as error1:
-        print('An error occurred: %s' % error1)
+        print 'An error occurred: %s' % error1
 
 
 def get_message(service1, user_id, msg_id):
@@ -102,44 +100,94 @@ def get_message(service1, user_id, msg_id):
     """
     try:
         message = service1.users().messages().get(userId=user_id, id=msg_id, format='full').execute()
-
-        # print('Message snippet: %s' % message['snippet'])
-
         return message
     except errors.HttpError as error1:
-        print('An error occurred: %s' % error1)
+        print 'An error occurred: %s' % error1
 
 
-def read_space_separated(f):
-    for line in f:
+def read_space_separated(input_file):
+    for line in input_file:
         line_list = []
         for token in line.split():
-            line_list.append(token)
-        line_tuple = tuple(line_list)
-        del line_list
-        data_list.append(line_tuple)
+            line_list.append(float(token))
+        data_list.append(line_list)
 
 
-startTime = time.time()
 print 'Reading Dataset'
 data_list = []
 # noinspection SpellCheckingInspection
 with open('spambase.data', 'r') as data_file:
     read_space_separated(data_file)
 
-data_tuple = tuple(data_list)
-del data_list
-print len(data_tuple), 'lines read successfully in', time.time() - startTime, 's'
+print len(data_list), 'entries read from dataset'
 
-print('Starting Gmail API...\nPlease Wait...')
+print 'Retrieving Email List'
 idList = list_messages(service, 'me')
-emailContent = []
-success = 0
-error = 0
-print('Reading email...')
-for j in range(0, len(idList), 1):
+print 'Email list retrieved'
+
+emailsList = []
+
+columnNames = []
+
+with open('spambase.names') as f:
+    for i in f:
+        columnNames.append(i.replace('\n', ''))
+
+datasetDivisionDict = dict((x, []) for x in columnNames)  # {'attribute_name':[q1,mean,q3,max_value]}
+
+for i in xrange(0, len(columnNames)):
+    q1 = 0.0
+    count_q1 = 0
+    mean = 0.0
+    q3 = 0
+    count_q3 = 0.0
+    max_value = 0.0
+    for j in xrange(0, len(data_list)):
+        mean += data_list[j][i]
+    mean /= len(data_list)
+
+    for j in xrange(0, len(data_list)):
+        if 0 <= data_list[j][i] < mean:
+            q1 += data_list[j][i]
+            count_q1 += 1
+    q1 /= count_q1
+
+    for j in xrange(0, len(data_list)):
+        if data_list[j][i] >= mean:
+            q3 += data_list[j][i]
+            count_q3 += 1
+    q3 /= count_q3
+
+    for j in xrange(0, len(data_list)):
+        if max_value < data_list[j][i]:
+            max_value = data_list[j][i]
+
+    datasetDivisionDict[columnNames[i]] = [q1, mean, q3, max_value]
+
+for i in xrange(0, len(data_list)):
+    for j in xrange(0, len(columnNames)):
+        if 0.0 <= data_list[i][j] < datasetDivisionDict[columnNames[j]][0]:
+            data_list[i][j] = 1
+        elif datasetDivisionDict[columnNames[j]][0] <= data_list[i][j] < datasetDivisionDict[columnNames[j]][1]:
+            data_list[i][j] = 2
+        elif datasetDivisionDict[columnNames[j]][1] <= data_list[i][j] < datasetDivisionDict[columnNames[j]][2]:
+            data_list[i][j] = 3
+        elif datasetDivisionDict[columnNames[j]][2] <= data_list[i][j] < datasetDivisionDict[columnNames[j]][3]:
+            data_list[i][j] = 4
+        else:
+            data_list[i][j] = 5
+
+for i in data_list:
+    if int(i[len(data_list[0]) - 1]) == 1:
+        i[len(data_list[0]) - 1] = True
+    else:
+        i[len(data_list[0]) - 1] = False
+
+print data_list[0]
+
+for j in xrange(0, len(idList)):
     idDict = idList[j]
-    # print(idDict['id'])
+    print 'Reading email with id', idDict['id']
     try:
         msgDict = get_message(service, 'me', idDict['id'])
         headersList = msgDict['payload']['headers']
@@ -147,28 +195,31 @@ for j in range(0, len(idList), 1):
         for i in range(0, len(headersList), 1):
             if headersList[i]['name'] == 'Subject':
                 indexSubject = i
-        indexParts = 0
 
         body = ''
-        if 'body' in msgDict['payload'] and 'parts' not in msgDict['payload']:
+        if 'body' in msgDict['payload'] and msgDict['payload']['mimeType'] == 'text/plain':
             body += msgDict['payload']['body']['data']
-        elif 'parts' in msgDict['payload']['parts'][0]:
-            if 'data' in msgDict['payload']['parts'][0]['parts'][0]['body']:
-                body += msgDict['payload']['parts'][0]['parts'][0]['body']['data']
-            elif 'parts' in msgDict['payload']['parts'][0]['parts'][0]:
-                body += msgDict['payload']['parts'][0]['parts'][0]['parts'][0]['body']['data']
-        else:
+        elif 'parts' in msgDict['payload'] and msgDict['payload']['parts'][0]['mimeType'] == 'text/plain':
             body += msgDict['payload']['parts'][0]['body']['data']
+        elif 'parts' in msgDict['payload']['parts'][0] and \
+                        msgDict['payload']['parts'][0]['parts'][0]['mimeType'] == 'text/plain':
+            body += msgDict['payload']['parts'][0]['parts'][0]['body']['data']
+        elif 'parts' in msgDict['payload']['parts'][0]['parts'][0] and \
+                        msgDict['payload']['parts'][0]['parts'][0]['parts'][0]['mimeType'] == 'text/plain':
+            body += msgDict['payload']['parts'][0]['parts'][0]['parts'][0]['body']['data']
+        else:
+            print 'error'
 
         body = str(base64.b64decode(str(body).replace('-', '+').replace('_', '/')))
         body = body.decode('utf-8')
-        # print body
-        # body = (body[2:len(body) - 1])
-        body = body.replace('\\r\\n', '\\n')
-        body = body.replace('\\n', '\n')
+        # body = body.replace('\\r\\n', '\\n')
+        # body = body.replace('\\n', '\n')
         subject = headersList[indexSubject]['value']
-        emailContent.append(subject + '\t' + body + '\n')
         email = subject + '\t' + body + '\n'
+
+        if len(body) > 0:
+            emailsList.append(email)
+
         temp = ''
         for c in email:
             if c.isalnum() or c.isspace():
@@ -176,19 +227,57 @@ for j in range(0, len(idList), 1):
             else:
                 temp += ' '
         total_words = len(temp.split())
-        print temp
-        words = ['make']
-        count_make = dict((x, 0) for x in words)
+        words = ['make', 'address', 'all', '3d', 'our', 'over', 'remove', 'internet', 'order', 'mail', 'receive',
+                 'will', 'people', 'report', 'addresses', 'free', 'business', 'email', 'you', 'credit', 'your', 'font',
+                 '000', 'money', 'hp', 'hpl', 'george', '650', 'lab', 'labs', 'telnet', '857', 'data', '415', '85',
+                 'technology', '1999', 'parts', 'pm', 'direct', 'cs', 'meeting', 'original', 'project', 're', 'edu',
+                 'table', 'conference']
+        count_words = dict((x, 0) for x in words)
         for w in re.findall(r"\w+", temp):
-            if w in count_make:
-                count_make[w] += 1
-        print idList[j]['id'], total_words,count_make
-        success += 1
+            if w in count_words:
+                count_words[w] += 1
+        total_chars = 0
+        for i in email:
+            if not i == ' ':
+                total_chars += 1
+        chars = [';', '(', '[', '!', '$', '#']
+        count_chars = dict((x, 0) for x in chars)
+        for w in email:
+            if w in count_words:
+                count_chars[w] += 1
+        percent_words = dict((x, 0) for x in words)
+        for k in percent_words.iterkeys():
+            percent_words[k] = 100.0 * float(count_words[k]) / float(total_words)
+        percent_chars = dict((x, 0) for x in chars)
+        for k in percent_chars.iterkeys():
+            percent_chars[k] = 100.0 * float(count_chars[k]) / float(total_chars)
+
+        attributeValueList = []
+
+        for i in xrange(0, len(words)):
+            attributeValueList.append(percent_words[words[i]])
+
+        for i in xrange(0, len(chars)):
+            attributeValueList.append(percent_chars[chars[i]])
+
+        for i in xrange(0, len(columnNames)):
+            if 0.0 <= attributeValueList[i] < datasetDivisionDict[columnNames[i]][0]:
+                attributeValueList[i] = 1
+            elif datasetDivisionDict[columnNames[i]][0] <= attributeValueList[i] < \
+                    datasetDivisionDict[columnNames[i]][1]:
+                attributeValueList[i] = 2
+            elif datasetDivisionDict[columnNames[i]][1] <= attributeValueList[i] < \
+                    datasetDivisionDict[columnNames[i]][2]:
+                attributeValueList[i] = 3
+            elif datasetDivisionDict[columnNames[i]][2] <= attributeValueList[i] < \
+                    datasetDivisionDict[columnNames[i]][3]:
+                attributeValueList[i] = 4
+            else:
+                attributeValueList[i] = 5
+
+        print attributeValueList
+
     except Exception as e:
         print 'Error in id %s' % idList[j]['id']
         print e
-        error += 1
         pass
-    print('\t%f%% complete\t\tsuccess %%age: %f%%\t\telapsed time: %f min' % (
-        float((j + 1) * 100) / float(len(idList)), float(success * 100) / float(success + error),
-        (time.time() - startTime) / 60))
